d78e27abfcdd5eaede0cc473abeacdd68bf17182
Nasser <nasser-silva@hotmail.com>
2024-11-15 14:50:36 -0300

    Add env api key

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,7 +5,9 @@ const BASE_URL = "https://sky-scrapper.p.rapidapi.com/api/v1/flights/"
 const flight_base = axios.create({
     baseURL: BASE_URL,
     timeout: 5000,
-    headers: { 'x-rapidapi-key': '' }
+    headers: {
+        'x-rapidapi-key': import.meta.env.VITE_RAPID_API_KEY,
+    }
 });
 
 export const flightApi = {
