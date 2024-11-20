7635ece9c5e6010762f8b705d8a7c7b00f97d115
Nasser <nasser-silva@hotmail.com>
2024-11-20 15:35:59 -0300

    Remove axios timeout

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,7 @@ const BASE_URL = "https://sky-scrapper.p.rapidapi.com/api/v1/flights/"
 
 const flight_base = axios.create({
     baseURL: BASE_URL,
-    timeout: 5000,
+    // timeout: 5000,
     headers: {
         'x-rapidapi-key': import.meta.env.VITE_RAPID_API_KEY,
     }
